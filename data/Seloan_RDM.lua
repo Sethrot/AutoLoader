da389d7d87ebedabd2b06d13dc9913d032d885cd
neatmachine <mitch.trahan@gmail.com>
2025-11-16 17:28:24 -0800

    simpler

--- data/Seloan_RDM.lua
+++ data/Seloan_RDM.lua
@@ -1,9 +1,6 @@
 -- Seloan_RDM.lua
 -- Basic Red Mage (RDM) configuration file with AutoLoader support
 local autoloader = require("autoloader")
-local common_job = require("common_job")
-local codex = require("autoloader-codex")
-local log = require("autoloader-logger")
 autoloader.lockstyle = 20
 autoloader.auto_movement = "on"
 autoloader.register_keybind("^F10", "input //ez cycle back")
